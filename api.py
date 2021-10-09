37dbffe6874babddc667aac5bf8147e3265e624e
smailzitan <zitansmail@gmail.com>
2021-10-09 20:18:05 +0200

    fick pickle problem

--- api.py
+++ api.py
@@ -5,13 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    filename = 'BostonHousePricing.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-    rm ,ptratio, lstat = request.args.get('rm'), request.args.get('ptratio'), request.args.get('lstat')
-    x = np.array([rm, ptratio, lstat]).reshape(1,3)
-    return str(loaded_model.predict(x)[0])
-    #return 'works'
-
-
-if __name__ == "__main__":
-    app.run()
+    try:
+        filename = 'BostonHousePricing.sav'
+        loaded_model = pickle.load(open(filename, 'rb'))
+        rm ,ptratio, lstat = request.args.get('rm'), request.args.get('ptratio'), request.args.get('lstat')
+        x = np.array([rm, ptratio, lstat]).reshape(1,3)
+        return str(loaded_model.predict(x)[0])
+    except Exception as ex:
+        return str(ex)
